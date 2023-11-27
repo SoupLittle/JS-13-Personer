55adc64dbed781dd0e9863ad4afe055f959463c2
SoupLittle <marleneturlid@gmail.com>
2023-11-27 09:52:27 +0100

    .

---
+++ index.js
@@ -0,0 +1,52 @@
+const ansatt = [
+    {
+      name: "Laila Johanne Handelsby",
+      role: "Rektor",
+      image: "Bildet"
+    },
+    {
+      name: "Torben Andersen",
+      role: "Assisterende rektor",
+      image: "Bildet"
+    },
+    {
+      name: "Martin Sandø-Frank",
+      role: "Studieleder",
+      image: "Bildet"
+    },
+    {
+      name: "Cathrine Mannsaker",
+      role: "Undervisningsleder",
+      image: "Bildet"
+    },
+    {
+      name: "Anette Øwre Bollvåg",
+      role: "Undervisningsleder",
+      image: "Bildet"
+    },
+    {
+      name: "Anne Raastad-Hoel",
+      role: "Undervisningsleder",
+      image: "Bildet"
+    },
+    {
+      name: "Erik Bergerud",
+      role: "Undervisningsleder",
+      image: "Bildet"
+    },
+    {
+      name: "Camilla Engberg",
+      role: "Undervisningsleder",
+      image: "Bildet"
+    },
+    {
+      name: "Sissel Moen",
+      role: "Administrasjonsleder",
+      image: "Bildet"
+    }
+  ];
+
+//   console.log(ansatt[8]); 
+
+
+// document.write(ansatt[8])
