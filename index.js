b9224fa1026cc7e7cf330aaf11e9560082b7afed
SoupLittle <marleneturlid@gmail.com>
2023-11-29 10:39:47 +0100

    .

--- index.js
+++ index.js
@@ -1,87 +1,126 @@
-const ansatt = [
+var ansatt = [
   {
     name: "Laila Johanne Handelsby",
     role: "Rektor",
-    image: "img/rektor-laila-handelsby.jpg"
+    image: "img/rektor-laila-handelsby.jpg",
+    text:  "Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Torben Andersen",
     role: "Assisterende rektor",
-    image: "img/torben-gjerstad.jpg"
+    image: "img/torben-gjerstad.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Martin Sandø-Frank",
     role: "Studieleder",
-    image: "img/martin-frank.jpg"
+    image: "img/martin-frank.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Cathrine Mannsaker",
     role: "Undervisningsleder",
-    image: "img/cathrine-mannsaaker.jpg"
+    image: "img/cathrine-mannsaaker.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Anette Øwre Bollvåg",
     role: "Undervisningsleder",
-    image: "img/anette-owre-bollvag.jpg"
+    image: "img/anette-owre-bollvag.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Anne Raastad-Hoel",
     role: "Undervisningsleder",
-    image: "img/anne-raastad-hoel.jpg"
+    image: "img/anne-raastad-hoel.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Erik Bergerud",
     role: "Undervisningsleder",
-    image: "img/erik-bergerud.jpg"
+    image: "img/erik-bergerud.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Camilla Engberg",
     role: "Undervisningsleder",
-    image: "img/camilla-engberg.jpg"
+    image: "img/camilla-engberg.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   },
   {
     name: "Sissel Moen",
     role: "Administrasjonsleder",
-    image: "img/sissel-moen.jpg"
+    image: "img/sissel-moen.jpg",
+    text:"Integer pretium justo a nisi rutrum, a ultricies eros dictum. Nunc rhoncus lacinia eros. Aenean nec ultricies sem, maximus mattis nibh."+ "\r\n" +"In tincidunt turpis pulvinar nisi lobortis aliquam. Sed nec orci quis elit placerat vehicula eget quis lorem. Sed sagittis vulputate augue."+ "\r\n" +"Duis tincidunt gravida turpis, in pellentesque sapien convallis vitae. Mauris luctus purus ante, ut aliquet elit ultricies et."+ "\r\n" +"Vestibulum scelerisque, nulla eu tempor auctor, diam dolor efficitur mi, in aliquet nisl elit at ex."+ "\r\n" +"Etiam erat dolor, tempus vitae mauris at, ornare mollis sem. Integer non ex eu tellus elementum semper in in justo."
   }
 ];
 
 
-TODO:
+document.getElementById('name').addEventListener('input', function (event) {
+  var inputName = this.value.trim().toLowerCase();
 
-document.getElementById('name').addEventListener('input', function () {
-  const inputName = this.value.trim().toLowerCase();
 
-  // Find the matching person in the array
-  const matchingPerson = ansatt.find(person => person.name.toLowerCase() === inputName);
+  if (event.key === 'Enter') {
+    processInput(inputName);
+  }
+});
 
-  // Update the result container
-  const resultContainer = document.getElementById('resultContainer');
+document.getElementById('name').addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); 
+    var inputName = this.value.trim().toLowerCase();
+    processInput(inputName);
+  }
+});
+
+ 
+function processInput(inputName) {
+  var matchingPerson = ansatt.find(person => {
+    var fullName = person.name.toLowerCase();
+    return fullName.includes(inputName);
+})
+
+  var resultContainer = document.getElementById('resultContainer');
   resultContainer.innerHTML = '';
 
   if (matchingPerson) {
 
 
-    const resultImage = document.createElement('img')
+    var resultImage = document.createElement('img')
     resultImage.src = matchingPerson.image
     resultImage.alt = matchingPerson.name
     resultImage.className = 'result-image'
 
-    const resultName = document.createElement('div')
+    var resultName = document.createElement('div')
     resultName.textContent = matchingPerson.name
     resultName.className = 'result-name'
 
-    const resultRole = document.createElement('div')
+    var resultRole = document.createElement('div')
     resultRole.textContent = matchingPerson.role
     resultRole.className = 'result-role'
 
-    // const resultText = document.createElement('text')
-    // resultRole.textContent = matchingPerson.text
-    // resultRole.className ='result-text'
+    // var resultText = document.createElement('div')
+    // resultText.textContent = matchingPerson.text
+    // resultText.className ='result-text'
+
+    var resultText = document.createElement('div');
+    resultText.innerHTML = matchingPerson.text.replace(/\r\n/g, '<br>');
+    resultText.className = 'result-text';
+    
+    
 
     resultContainer.appendChild(resultImage);
     resultContainer.appendChild(resultName);
     resultContainer.appendChild(resultRole);
-    // resultContainer.appendChild(resultText)
+    resultContainer.appendChild(resultText)
+    
+    var elements = document.querySelectorAll("input");
+    
+    for(var element of elements) {
+    element.value = "";
+  }} else {
+    alert('No match found! Try again with a different name')
   }
-});
+
+  }
+
